63c5e2aba509257260fe40023131e0292c15e342
PGabo <papgabox99@gmail.com>
2019-12-02 15:08:42 +0100

    loginErrorMessage funkció implementálva

--- web/functions.php
+++ web/functions.php
@@ -14,3 +14,17 @@ function exitAlert($alert)
             alert('$alert');
         </script>");
 }
+function loginErrorMessage()
+{
+    exitAlertRedirect('Helytelen felhasználónév vagy jelszó!', 'index.php');
+}
+function isProperAddress($str)
+{
+    if ($str == null || !is_string($str))
+        return false;
+    else
+    {
+        preg_match('@\s[0-9]{1,3}(\/\w)?\.?$@', $str, $matches);
+        return !empty($matches);
+    }
+}
