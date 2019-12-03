3c142471cdd99579a4db05cdd701559dcd694be4
PGabo <papgabox99@gmail.com>
2019-12-03 10:58:44 +0100

    Email verifikálás felépítése, javítása

--- web/email_verification.php
+++ web/email_verification.php
@@ -9,3 +9,29 @@ if (!empty($_GET['code']) && isset($_GET['code'])) {
      if(!($account->Activate($_GET['code'])))  exitAlertRedirect('Sikertelen aktiválás', 'index.php');
 }
 ?>
+<!DOCTYPE html>
+<html lang="hu">
+<?php require ('./html/head.html');?>
+<body>
+<?php require('nav.php');?>
+<div class="container-fluid">
+    <div class="col-sm-6">
+        <div class="row">
+            <div class="col-xs-12">
+                <h3>Verifikálta e-mail címét </h3>
+                <hr>
+                <p></p>
+                <p> Most már be tud jelentkezni.</p>
+                <p>A bejelentkezéshez <a href="login.php">kattintson ide</a>.</p>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <div class="col-sm-4">
+        <div class="panel panel-default">
+        </div>
+    </div>
+    <hr>
+</div>
+<?php require('./html/footer.html'); ?>
+</html>
